f8c9fe1896f265816755c98989044792d355eb2c
Vishal Panchal <77235447+Vishal-Panchal-DTU@users.noreply.github.com>
2021-01-10 06:26:12 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
-/* My First Project */
 #include <stdio.h>
 int main()
 {
-    printf("I Am Vishal Panchal");
-    return 0;
+  int num1,num2,num3,num4,num5,avg;   /* Declartion Of Variables */
+  scanf("%d",&num1);         /*  Input From User */
+  scanf("%d",&num2);         /*  Input From User */
+  scanf("%d",&num3);         /*  Input From User */
+  scanf("%d",&num4);         /*  Input From User */
+  scanf("%d",&num5);         /*  Input From User */
+  avg = (num1 + num2 + num3 + num4 + num5)/5;              
+  printf("The Average Of These 5 Numbers Is %d",avg);
+  return 0;
 }
